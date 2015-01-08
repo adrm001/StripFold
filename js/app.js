593273bf448985044443122ee20bd9918ab69383
Adrian <adr.mayorga@gmail.com>
2015-01-07 18:29:37 -0600

    add bootstrap, and angular first pass

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var app = (function () {
     var $AM = [[0, 0], [49, -170], [100, 0], [150, -30], [125, -170], [75, -50], [80, -58], [10, -58]];
     var $primaryDark = window.getComputedStyle(document.getElementById('brand-dark7'), null).color;//'#B2DFDB';
     var $primaryLight = window.getComputedStyle(document.getElementById('brand-light1'), null).color;//'#0094B2';
+    var $AMLogo;
 
     var app = angular.module('main', ['ngRoute']);
     //define routes
@@ -11,16 +12,34 @@ var app = (function () {
                 when('/home', {
                     templateUrl: 'partials/about.html'
                 }).
+                when('/projects', {
+                    templateUrl: 'partials/projects.html'
+                }).
+                when('/resume', {
+                    templateUrl: 'partials/resume.html'
+                }).
+                when('/contact', {
+                    templateUrl: 'partials/contact.html'
+                }).
                 otherwise({
                     redirectTo: '/home'
                 });
         }
     ]);
 
+    app.controller('NavCtl',['$scope',
+        function ($scope) {
+            $scope.tab = 'home';
+            $scope.select = function(tab){
+                $scope.tab = tab;
+            }
+        }
+    ]);
+
     app.controller('ViewCtl',['$scope',
         function($scope){
             $scope.$on('$viewContentLoaded',function(){
-                app.addLogo('AMLogoDiv');
+                $AMLogo = app.addLogo('AMLogoDiv');
             })
     }]);
 
